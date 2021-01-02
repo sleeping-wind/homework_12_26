f626a47b8401314b787512f3dd4ce3901329286f
sleeping-wind <842700325@qq.com>
2021-01-02 18:28:18 +0800

    添加多次点击功能

---
+++ HomeWork_12_26/src/com/example/homework_12_26/Hello.java
@@ -0,0 +1,14 @@
+package com.example.homework_12_26;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Hello extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.hello);
+	
+	}
+}
--- HomeWork_12_26/src/com/example/homework_12_26/MainActivity.java
+++ HomeWork_12_26/src/com/example/homework_12_26/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.homework_12_26;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -11,6 +12,7 @@ import android.widget.Button;
 public class MainActivity extends Activity {
 	private Button b1;
 	private Button b2;
+	private Button b3;
 	private int sum = 0;
 
 	@Override
@@ -43,6 +45,21 @@ public class MainActivity extends Activity {
 				Log.d("MainActivity", s);
 			}
 		});
-
+		b3 = (Button) findViewById(R.id.bt_duo);
+		b3.setOnClickListener(new OnClickListener() {
+		long[] mHits = new long[5]; //需要监听几次点击事件数组的长度就为几
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				//将mHints数组内的所有元素左移一个位置
+                System.arraycopy(mHits,1,mHits,0,mHits.length - 1);
+                //获得当前系统已经启动的时间
+                mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+                //点击的时间差小于500毫秒就认 为是连续点击
+                if (mHits[0] >= (SystemClock.uptimeMillis() - 1000)){
+                    startActivity(new Intent(MainActivity.this,Hello.class));
+                }
+			}
+		});
 	}
 }
